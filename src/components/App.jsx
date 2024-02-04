a4ea115f1011c97f126da27ec7aaf4ba17741923
SerhiiAnd <136363351+SerhiiAnd@users.noreply.github.com>
2024-02-04 23:13:15 +0100

    save04.02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,28 +4,6 @@ import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-// class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   handleFeedback = type => {
-//     this.setState(prevState => ({ [type]: prevState[type] + 1 }));
-//   };
-
-//   countTotalFeedback = () => {
-//     const { good, neutral, bad } = this.state;
-//     return good + neutral + bad;
-//   };
-
-// countPositiveFeedbackPercentage = () => {
-//   const total = this.countTotalFeedback();
-//   const { good } = this.state;
-//   return total > 0 ? Math.round((good / total) * 100) : 0;
-// };
-
 const App = () => {
   const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
   const handleFeedback = type => {
@@ -47,19 +25,6 @@ const App = () => {
   };
 
   const totalFeedback = countTotalFeedback();
-
-  // render() {
-  //   const { good, neutral, bad } = this.state;
-  //   const totalFeedback = this.countTotalFeedback();
-
-  //   return (
-  //     <div className="feedback-options-container">
-  //       <Section title="Please leave feedback">
-  //         <FeedbackOptions
-  //           options={['good', 'neutral', 'bad']}
-  //           onLeaveFeedback={this.handleFeedback}
-  //         />
-  //       </Section>
 
   return (
     <div className="feedback-options-container">
